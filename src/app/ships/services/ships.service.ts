8b5fa7ab819e48437f368cd617e1bc643ff213d4
Joseba <mjoseba@gmail.com>
2020-08-28 18:52:44 +0200

    Cache with 5min the ship API

--- src/app/ships/services/ships.service.ts
+++ src/app/ships/services/ships.service.ts
@@ -1,43 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
-import { of ,  throwError as _throw ,  Observable } from 'rxjs';
-import { switchMap ,  catchError } from 'rxjs/operators';
-import {environment} from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { take, shareReplay} from 'rxjs/operators';
+import { environment} from '../../../environments/environment';
+import { CachedHttpClient} from './CachedHttpClient';
 
 @Injectable()
 export class ShipsService {
 
   constructor(
-    private http: HttpClient,
+    private httpCached: CachedHttpClient,
   ) { }
 
   loadShips(payload?: { page: number }): Observable<any> { // todo: create ship model
-    return this.http.get(
-      environment.api.ships + '?page=' + payload.page
-    );
+    return this.httpCached.get(environment.api.ships + '?page=' + payload.page, 300000);
   }
 
   loadShipDetails(id: number): Observable<any> { // todo: create ship model
-    return this.http.get(
-      environment.api.ships + id
-    );
-  }
-
-  private _manageLoad() {
-    return (obs: Observable<any>) => {
-      return obs.pipe(
-        catchError(e => {
-          return _throw('Error de conectividad');
-        }),
-        switchMap(data => {
-
-          console.log(data);
-
-          if (data.status === 200 || data.status === 204 ) {
-            return of(data);
-          }
-        })
-      );
-    };
+    return this.httpCached.get(environment.api.ships + id, 300000);
   }
 }
