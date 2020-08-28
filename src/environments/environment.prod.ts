23b8537ae70dd31404f2a1c8a810a368f979916d
Joseba <mjoseba@gmail.com>
2020-08-28 17:53:09 +0200

    /dist removed from gitignore to create visible page in github git production build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
+const baseUrl = 'https://swapi.dev/api';
+
 export const environment = {
-  production: true
+  production: true,
+  api: {
+    login: `${baseUrl}/login`, // endpoint doesn't exists
+    register: `${baseUrl}/register`, // endpoint doesn't exists
+    ships: `${baseUrl}/starships/`,
+    films: `${baseUrl}/films/`,
+    pilot: `${baseUrl}/people/`,
+  }
 };
